c494887cb3fa62fafb68f1068d1e6778997c411c
fabian4 <fabian4@163.com>
2021-11-18 17:08:23 +0800

    update push module

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"github.com/fabian4/Fyi_sever/routers"
 	"github.com/gin-gonic/gin"
+	"github.com/go-ini/ini"
 )
 
 func main() {
 	r := gin.Default()
 	routers.InitPushRouter(r)
-	if err := r.Run(":3000"); err != nil {
+	cfg, _ := ini.Load("config.ini")
+	port := ":" + cfg.Section("sever").Key("port").String()
+	if err := r.Run(port); err != nil {
 		fmt.Printf("startup service failed, err:%v\n\n", err)
 	}
 }
